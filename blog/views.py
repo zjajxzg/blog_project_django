b237d83f43161d781311e530273af6ae80e6281e
zjajxzg <zjajxzg@163.com>
2018-02-26 17:28:59 +0800

    first commit

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+from .models import Post
+from .models import Post
+
+# Create your views here.
+def index(request):
+    #return HttpResponse("欢迎访问我的博客主页！")
+    #return render(request,'blog/index.html',context = {'title':'我的博客首页','welcome':'欢迎访问我的博客首页'})
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request,'blog/index.html',context = {'post_list':post_list})
+
+def detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/detail.html',context={'post':post})
+
+
+
+
