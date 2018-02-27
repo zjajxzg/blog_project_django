ffaa83f750b00b6386906e451782687b8766996a
zjajxzg <zjajxzg@163.com>
2018-02-27 17:22:36 +0800

    second commit

--- blog/models.py
+++ blog/models.py
@@ -16,8 +16,7 @@ class Category(models.Model):
     name = models.CharField(max_length = 100)
     def __str__(self):
         return self.name
-    def get_absolute_url(self):
-        return reverse('blog:detail',kwargs={'pk':self.pk})
+
 
 class Tag(models.Model):
     """
@@ -64,3 +63,5 @@ class Post(models.Model):
     author = models.ForeignKey(User)
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:detail',kwargs={'pk':self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
-from .models import Post
+import markdown
 
 # Create your views here.
 def index(request):
@@ -12,6 +12,12 @@ def index(request):
 
 def detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
     return render(request,'blog/detail.html',context={'post':post})
 
 
