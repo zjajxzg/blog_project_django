61316ce1f9b194363c3886ee0aeaffb6f763271d
zjajxzg <zjajxzg@163.com>
2018-03-13 17:13:20 +0800

    2018-03-13

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from comments.forms import CommentForm
 from django.http import HttpResponse
 from .models import Post, Category, Tag
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView, DetailView
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+from django.db.models import Q
 import markdown
 
 # Create your views here.
@@ -93,13 +96,14 @@ class PostDetailView(DetailView):
         return response
 
     def get_object(self, queryset=None):
-        post = super(PostDetailView, self).get_object(queryset=None)
-        post.body = markdown.markdown(post.body,
-                                      extensions=[
+        post = super(PostDetailView,self).get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
                                           'markdown.extensions.extra',
                                           'markdown.extensions.codehilite',
-                                          'markdown.extensions.toc',
+                                          TocExtension(slugify=slugify),
                                       ])
+        post.body = md.convert(post.body)
+        post.toc = md.toc
         return post
 
     def get_context_data(self, **kwargs):
@@ -121,6 +125,19 @@ class TagView(ListView):
     def get_queryset(self):
         tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
         return super(TagView, self).get_queryset().filter(tags=tag)
+
+
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
+
+    if not q:
+        error_msg = "请输入关键字"
+        return render(request, 'blog/index.html', {'error_msg': error_msg})
+
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'error_msg': error_msg,
+                                               'post_list': post_list})
 
 
 def index(request):
